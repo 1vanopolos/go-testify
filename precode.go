e86b1a33aaaa1033228f1596850d396ba0c64184
1vanopolos <prichak7@gmail.com>
2024-10-28 08:59:26 +0300

    second

--- precode.go
+++ precode.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -47,25 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	//проверяю код запроса 200
-	require.Equal(t, responseRecorder.Code, 200)
-	//Проверяю на правильность заполненеия города
-	require.Equal(t, responseRecorder.Code, 400)
-	require.Equal(t, responseRecorder.Body, "wrong city value")
-	//проверяю тело запроса на заполненность
-	assert.NotEmpty(t, responseRecorder.Body)
-	//получаю параметр count
-	count := req.URL.Query().Get("count")
-	//сравниваю totalCount и count
-	assert.Equal(t, totalCount, count)
 }
---
+++ precode_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := len(cafeList["moscow"])
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	//проверяю код запроса 200
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
+	//Проверяю на правильность заполненеия города
+	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+	//проверяю тело запроса на заполненность
+	assert.NotEmpty(t, responseRecorder.Body)
+	//получаю параметр count
+	count := req.URL.Query().Get("count")
+	//сравниваю totalCount и count
+	assert.Equal(t, totalCount, count)
+}
