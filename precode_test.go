b3a5a3db85e29068a52aea28acb480b0cdd2d3e5
1vanopolos <prichak7@gmail.com>
2024-10-28 09:34:32 +0300

    third

--- precode_test.go
+++ precode_test.go
@@ -10,6 +10,7 @@ import (
 )
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+
 	totalCount := len(cafeList["moscow"])
 	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
 
@@ -17,12 +18,17 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
+	//Первая проверка
 	//проверяю код запроса 200
 	require.Equal(t, responseRecorder.Code, http.StatusOK)
-	//Проверяю на правильность заполненеия города
-	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
 	//проверяю тело запроса на заполненность
 	assert.NotEmpty(t, responseRecorder.Body)
+
+	//Вторая проверка
+	//Проверяю на правильность заполненеия города
+	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+
+	//Третья проверка
 	//получаю параметр count
 	count := req.URL.Query().Get("count")
 	//сравниваю totalCount и count
